788856174c98602e098c762a5a61f1aea3d3ee48
wghmyx <wghmyx@gmail.com>
2023-06-24 14:43:50 -0700

    Adding images

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -23,5 +23,9 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	//为了让程序访问到次文件夹
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
